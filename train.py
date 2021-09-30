8cf8e61af452df3e899f3cc99c6ad28eaf3ecc9d
Chen <xuweichen1999@126.com>
2021-09-30 20:10:21 +0800

    1.I add a print in train.py again.

--- train.py
+++ train.py
@@ -8,4 +8,5 @@ from basicsr.train import train_pipeline
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir))
     print('aa')
+    print('bb')
     train_pipeline(root_path)
